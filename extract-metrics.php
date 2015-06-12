5070fe9d945cdec38ac95b27aa10064e85a2c5b4
Jean-Sébastien Hedde <jshedde@lafourchette.com>
2015-06-12 11:21:47 +0200

    Configuration with cli args

--- extract-metrics.php
+++ extract-metrics.php
@@ -8,9 +8,24 @@ use Liuggio\StatsdClient\StatsdClient,
     Liuggio\StatsdClient\Factory\StatsdDataFactory,
     Liuggio\StatsdClient\Sender\SocketSender,
     Liuggio\StatsdClient\Service\StatsdService;
-            // use Liuggio\StatsdClient\Sender\SysLogSender;
 
-$sender = new SocketSender('delta1.lafourchette.lan', 8125, 'udp');
+// use Liuggio\StatsdClient\Sender\SysLogSender;
+
+$longopts  = array(
+    'statsd-host:',     // Valeur requise
+    'logs-directory:',
+    'project-prefix:'
+);
+$options = getopt(null, $longopts);
+
+foreach ($longopts as $opt) {
+    $opt = substr($opt, 0, -1);
+    if (false === array_key_exists($opt, $options)) {
+        throw new \RuntimeException(sprintf('"%s" is required.', $opt));
+    }
+}
+
+$sender = new SocketSender($options['statsd-host'], 8125, 'udp');
             // $sender = new SysLogSender(); // enabling this, the packet will not send over the socket
 
 $client  = new StatsdClient($sender);
@@ -19,19 +34,20 @@ $service = new StatsdService($client, $factory);
 
 $file = 'clover.xml';
 
-$prefix = 'lafourchette.ci.b2c.core.';
+$prefix = $options['project-prefix'];
+$logsDir = $options['logs-directory'];
 
 $metrics = array();
-$collector = new CloverXmlMetricCollector('clover.xml', $prefix . 'coverage.');
+$collector = new CloverXmlMetricCollector($logsDir . '/clover.xml', $prefix . 'coverage.');
 $metrics = array_merge($collector->collect(), $metrics);
 
-$collector = new JunitXmlMetricCollector('junit.xml', $prefix . 'phpunit.');
+$collector = new JunitXmlMetricCollector($logsDir . '/junit.xml', $prefix . 'phpunit.');
 $metrics = array_merge($metrics, $collector->collect());
 
-$collector = new CheckstyleXmlMetricCollector('checkstyle.xml', $prefix . 'checkstyle.');
+$collector = new CheckstyleXmlMetricCollector($logsDir . '/checkstyle.xml', $prefix . 'checkstyle.');
 $metrics = array_merge($metrics, $collector->collect());
 
-$collector = new PhplocXmlMetricCollector('phploc.xml', $prefix . 'phploc.');
+$collector = new PhplocXmlMetricCollector($logsDir . '/phploc.xml', $prefix . 'phploc.');
 $metrics = array_merge($metrics, $collector->collect());
 
 //var_dump($metrics);
